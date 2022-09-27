98dbf7623dd3c9b6c150346efa58107370e6c916
Mikael Lindeman <mikael.kalajalindeman@student.kyh.se>
2022-09-27 14:50:37 +0200

    update some more

--- js/app.js
+++ js/app.js
@@ -11,10 +11,70 @@ selections.forEach((selection) => {
         const cInput = cOptions[Math.floor(Math.random() * 3)];
 
         updateMoveImgs(pInput, cInput);
+        compareInputs(pInput, cInput);
+        updateScore();
+        if (checkWinner()) {
+            pScore = cScore = 0;
+            updateScore();
+        };
     })
 });
 
 function updateMoveImgs(pInput, cInput) {
     document.getElementById("p-move").src = `./img/${pInput}.svg` ;
     document.getElementById("c-move").src = `./img/${cInput}.svg` ;
+}
+
+function compareInputs (pInput, cInput) {
+    if (pInput === cInput ) {
+        document.getElementById("result-text").innerHTML = "Draw!";
+        return;
+    }
+
+    if(pInput === "rock") {
+        if(cInput == "scissors") {
+        document.getElementById("result-text").innerHTML = "You win!";
+        pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer wins!";
+            cScore++;
+        }
+    }
+    if(pInput === "paper") {
+        if(cInput == "rock") {
+        document.getElementById("result-text").innerHTML = "You win!";
+        pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer wins!";
+            cScore++;
+        }
+    }
+    if(pInput === "scissors") {
+        if(cInput == "paper") {
+        document.getElementById("result-text").innerHTML = "You win!";
+        pScore++;
+        }
+        else {
+            document.getElementById("result-text").innerHTML = "Computer wins!";
+            cScore++;
+        }
+    }
+}
+
+function updateScore() {
+    document.getElementById("p-score").innerHTML = pScore;
+    document.getElementById("c-score").innerHTML = cScore;
+}
+
+function checkWinner() {
+    if(pScore === 5 || cScore === 5) {
+        const winner = pScore === 5
+        ? "You won the game! Good job!"
+        : "Computer won the game, you lost!";
+        document.getElementById("result-text").innerHTML = winner;
+        return true;
+    }
+    return false;
 }
